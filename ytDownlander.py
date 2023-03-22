20fa362b2e44fb00c34673bf9563a355060f49bd
karolinafiema <128620768+karolinafiema@users.noreply.github.com>
2023-03-22 16:56:27 +0100

    Add files via upload

---
+++ ytDownlander.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+
+link = input("Enter link: ")
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+print("View: ", yt.views)
+time = yt.length // 60
+print("Length of video in minutes: ", time)
+yd = yt.streams.get_highest_resolution()
+
+print("Downloading...")
+yd.download('D:\Yt Filmy')
+print("Download completed")
+
